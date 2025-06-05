1eea71613fb087c5c6923fc621cd3ce5f37d90ae
GopiWebDev <gopisv.webdev@gmail.com>
2025-06-05 20:49:51 +0530

    add supabase url

--- src/main/java/com/gopisvdev/url_shortener/config/EnvConfig.java
+++ src/main/java/com/gopisvdev/url_shortener/config/EnvConfig.java
@@ -1,15 +1,11 @@
 package com.gopisvdev.url_shortener.config;
 
-
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class EnvConfig {
-    static {
-        Dotenv dotenv = Dotenv.configure().directory("./").load();
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-    }
-}
+//@Configuration
+//public class EnvConfig {
+//    static {
+//        Dotenv dotenv = Dotenv.configure().directory("./").load();
+//        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+//        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+//        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+//    }
+//}
